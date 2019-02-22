68e2d933bfa46f688c20e518785361486b07f155
Neos07 <neos07.simo@gmail.com>
2019-02-22 17:49:48 +0100

    Add project files.

---
+++ OSRS Hiscores Checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSRS_Hiscores_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
